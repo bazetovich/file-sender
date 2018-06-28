e2ed4c693731bbb19a3e130c60307eaff12aa115
i.kurski <ilya_snow@mail.ru>
2018-06-28 13:39:57 +0300

    up

--- scripts/toolbar-popup.js
+++ scripts/toolbar-popup.js
@@ -1,2 +1,22 @@
 const inputElement = document.querySelector('input[type="file"]');
 const pond = FilePond.create(inputElement);
+
+pond.setOptions({
+  name: "file",
+  server: {
+    url: "https://file.io",
+    process: {
+      url: "",
+      method: "POST",
+      withCredentials: false,
+      headers: {},
+      timeout: 7000,
+      onload: null,
+      onerror: null
+    },
+    fetch: null,
+    revert: null,
+    load: null,
+    restore: null
+  }
+});
